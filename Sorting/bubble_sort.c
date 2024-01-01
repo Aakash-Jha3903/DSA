c36b8c96c707bd48d27cb36ed479705dd34ba93f
Aakash <aakashjha343@gmail.com>
2024-01-01 11:18:03 +0530

    buble2

--- Sorting/bubble_sort.c
+++ Sorting/bubble_sort.c
@@ -1,5 +1,18 @@
-#include<stdio.h>
+//Bubble Sort in C
 
+void display_array(int *arr, int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("%d \t",arr[i]);
+    }
+    printf("\n");
+}
+void bubble_sort(int *arr,int n )
+{
+
+}
+#include<stdio.h>
 int main() {
     
     return 0;
